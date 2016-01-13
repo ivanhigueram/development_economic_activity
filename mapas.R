ae65fde3df44a83c24ea04f5e9af44876b949c23
ivanhigueram <ivanhigueram@gmail.com>
2016-01-13 14:39:03 -0500

    Several corrections - extraction to pixels rather than polygons.

--- mapas.R
+++ mapas.R
@@ -1,5 +1,8 @@
-#Maps
+library(dismo)
+library(rasterVis)
 
+#Maps
+setwd("~/Dropbox/BANREP/Pacifico/Primer_DTSER/Mapas_Graficos")
 #Map of communities in the Pacific littoral
 opar <- par()
 par(pin=c(1,2))
@@ -12,9 +15,13 @@ plot(communities_littoral[[1]], add=T, col="coral")
 plot(communities_littoral[[2]], add=T, col="coral4")
 
 #Map of lights over the Pacific littoral (rasters is a list of the rasters for the all the years)
-litoral_lights92 <- mask(rasters_pacifico[[35]], pacific_littoral_map)
+littoral_lights13 <- mask(rasters_pacifico[[35]], pacific_littoral_map)
 png("litoral_comunidades_2013.jpeg", width = 8.5, height = 11, units = 'in', res = 1000)
-plot(litoral_lights92, col=grey.colors(100))
+plot(littoral_lights13, col=grey.colors(100))
 plot(pacific_littoral_map, add=T)
-plot(communities_littoral[[1]], add=T, lwd=0.1)
+plot(communities_littoral[[2]], add=T, lwd=0.1, col="coral4", alpha=0.5)
+plot(black_territories_union, add=T, lwd=0.1, col="coral", alpha=0.5)
 dev.off()
+
+#Export to KML files
+lapply(rasters_pacifico, KML)
--- procesamiento.R
+++ procesamiento.R
@@ -1,9 +1,13 @@
 library(raster)
 library(rgdal)
+library(rasterVis)
 library(sp)
 library(ggplot2)
+library(rgdal)
+library(maptools)
 library(lattice)
 library(plyr)
+library(grid)
 library(dplyr)
 library(stringr)
 
@@ -12,23 +16,27 @@ setwd("/Volumes/LaCie/Datos")
 colombia_departments <- getData("GADM", download=T, country="CO", level=1)
 black_territories <- readOGR(dsn = "Comunidades", layer="Tierras de Comunidades Negras (2015) ")
 indigenous_territories <- readOGR(dsn = "Resguardos", layer="Resguardos Indigenas (2015) ") 
-elevation <- getData("alt", co="COL")
 
 #Get political GIS data
 landmines <- readOGR(dsn = "MAP&MUSE_GIS", layer = "eventos_shape")
 
-#Project CRS to black and indigenous territories
+#Project CRS to black and indigenous territories (1: blak territories, 2: indigenous territories)
 reproject_layers <- list(black_territories, indigenous_territories)
 layers_reprojected <- lapply(reproject_layers, spTransform, CRS=CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
-black_territories_reproject <- layers_reprojected[[1]]
-indigenous_territories_reproject <- layers_reprojected[[2]]
 
 #Select only departments over the pacific littoral (Chocó, Valle del Cauca, Cauca and Nariño)
 pacific_littoral <- c(11, 13, 21, 30)
 pacific_littoral_map <- colombia_departments[colombia_departments@data$ID_1 %in% pacific_littoral, ]
 
-#Filter only communities in the pacific lottoral
-communities_littoral <- sapply(layers_reprojected, crop, pacific_littoral_map)
+#Filter only communities in the pacific littoral
+communities_littoral <- lapply(layers_reprojected, crop, pacific_littoral_map)
+
+#Elevation data
+download.file(     
+url = "http://edcintl.cr.usgs.gov/downloads/sciweb1/shared/topo/downloads/GMTED/Global_tiles_GMTED/300darcsec/mea/W090/10S090W_20101117_gmted_mea300.tif" ,
+destfile = "altura_mean_30arc.tif", mode="wb")
+elevation_pacifico <- raster("altura_mean_30arc.tif")
+elevation_pacifico <- crop(elevation_pacifico, pacific_littoral_map)
 
 #Open .tif files as a raster (the raster package allow to read these files in the disk and not in the memory, this improves the efficiency of functions in R)
 setwd("~")
@@ -38,26 +46,26 @@ setwd("/Volumes/LaCie/NOAA2/TIFF/")
 list_raster <- list.files()
 rasters <- lapply(list_raster, raster)
 rasters_pacifico <- lapply(rasters, crop, pacific_littoral_map)
-
-#We need to put all rasters into the same extent (all have the same resolution)
-rasters_extent <- extent(rasters_pacifico[[1]])
+rasters_extent <- extent(rasters_pacifico[[1]]) #We need to put all rasters into the same extent (all have the same resolution)
+elevation_pacifico <- setExtent(elevation_pacifico, rasters_extent) #The same for elevation raster
 rasters_pacifico <- lapply(rasters_pacifico, setExtent, rasters_extent)
+stack_pacifico <- stack(rasters_pacifico) #Stack them!
 
-#Stack them
-stack_pacifico <- stack(rasters_pacifico)
+#Extract elevation and light data for each pixel (1*1 km  grid approximately)
+stack_pacifico_dataframe <- extract(stack_pacifico, seq_len(ncell(stack_pacifico)), df=TRUE)
+elevation_dataframe <- extract(elevation_pacifico, seq_len(ncell(elevation_pacifico)), df=TRUE)
+merge_rasters_dataframes <- merge(elevation_dataframe, stack_pacifico_dataframe, by="ID")
 
+#Join black communities territories (create a frontier)
+communities_littoral[[1]]$ID <- 1
+black_territories_union <- unionSpatialPolygons(communities_littoral[[1]], communities_littoral[[1]]$ID)
 
 #Now I extract the light info to the spatial polygons of the communities
 rasters_indigenas <- lapply(rasters_pacifico, raster::extract, communities_littoral[[2]], fun = mean, na.rm= TRUE, df = TRUE)
 rasters_communities <- lapply(rasters_pacifico, raster::extract,communities_littoral[[1]], fun = mean, na.rm= TRUE, df = TRUE)
-
 
 #Set the results as data.frame
 luces_promedio_indigenas <- data.frame(rasters_indigenas)
 luces_promedio_negritudes <- data.frame(rasters_communities)
 
 
-
-#Elevation data
-elevation_pacifico <- crop(elevation, pacific_littoral_map)
-elevation_pacifico <- setExtent(elevation_pacifico, rasters_extent)
