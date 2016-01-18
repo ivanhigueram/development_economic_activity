450894d97cc933dff57c61e01e3a99b798c4d1a3
ivanhigueram <ivanhigueram@gmail.com>
2016-01-18 16:31:55 -0500

    Changes to maps

--- mapas.R
+++ mapas.R
@@ -15,11 +15,11 @@ plot(communities_littoral[[1]], add=T, col="coral")
 plot(communities_littoral[[2]], add=T, col="coral4")
 
 #Map of lights over the Pacific littoral (rasters is a list of the rasters for the all the years)
-littoral_lights13 <- mask(rasters_pacifico[[35]], pacific_littoral_map)
+littoral_lights2013 <- mask(rasters_pacifico[[35]], pacific_littoral_map)
 png("litoral_comunidades_2013_municipios.jpeg", width = 8.5, height = 11, units = 'in', res = 1000)
-plot(rasters_pacifico[[35]], col=grey.colors(100))
-plot(layers_reprojected[[1]], add=T,  col= "coral")
-plot(pacific_littoral_map2, lwd=2, border= "red", lwd=1, add=T)
+plot(littoral_lights2013, col=grey.colors(100))
+plot(layers_reprojected[[1]], add=T,  boder="blue", lwd=)
+plot(pacific_littoral_map, lwd=2, border= "red", lwd=1, add=T)
 dev.off()
 
 #Export to KML files
